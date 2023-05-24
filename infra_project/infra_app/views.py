f508da70e7e4fc46d85149610ea45e13447288a8
Рачеев Артём <curtis1rachel@yandex.ru>
2023-05-24 22:52:56 +0300

    test with django test v.3

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,12 +1,9 @@
-"""Определение view-функций для приложения infra."""
 from django.http import HttpResponse
 
 
 def index(request):
-    """View-фунция первой страницы."""
     return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    """View-фунция второй страницы."""
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница!')
